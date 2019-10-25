42076c5b1fc324cf5eb14bef52dd218321f6bd8c
Maksym Kucher <the.stewk@gmail.com>
2019-10-25 09:14:26 -0700

    minor bugs fix added dependency required for mail logger.

--- src/main/java/com/maxkuchersiat/integration/IntegrationApplication.java
+++ src/main/java/com/maxkuchersiat/integration/IntegrationApplication.java
@@ -2,12 +2,20 @@ package com.maxkuchersiat.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class IntegrationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntegrationApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate()
+    {
+        return new RestTemplate();
     }
 
 }
